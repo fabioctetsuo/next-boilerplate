ae497e54664018eea74d4aab33e3d013ac683fc8
Fabio Tetsuo <fabioctetsuo@gmail.com>
2020-08-07 11:13:54 -0300

    feat: add some cool components at main

--- src/components/Main/__tests__/index.tsx
+++ src/components/Main/__tests__/index.tsx
@@ -6,10 +6,29 @@ describe('<Main />', () => {
     const { container } = render(<Main />);
     expect(container).toMatchInlineSnapshot(`
       <div>
-        <main>
-          <h1>
+        <main
+          class="styled__Wrapper-ngslp9-0 kCoOxt"
+        >
+          <img
+            alt="Imagem de um átomo e React Avançado escrito ao lado."
+            class="styled__Logo-ngslp9-1 hklGLi"
+            src="/img/logo.svg"
+          />
+          <h1
+            class="styled__Title-ngslp9-2 dOdxOs"
+          >
             React Avançado
           </h1>
+          <h2
+            class="styled__Description-ngslp9-3 ckgVfH"
+          >
+            TypeScript, ReactJS, NextJS e Styled Components
+          </h2>
+          <img
+            alt="Um desenvolvedor de frente para uma tela com código."
+            class="styled__Illustration-ngslp9-4 chTPnW"
+            src="/img/hero-illustration.svg"
+          />
         </main>
       </div>
     `);
@@ -20,5 +39,10 @@ describe('<Main />', () => {
     expect(
       getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument();
+  });
+
+  it('must render the correct background color', () => {
+    const { container } = render(<Main />);
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
   });
 });
