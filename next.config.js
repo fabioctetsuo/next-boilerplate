54c92b6a453c32c1148da3677bb2c25453ed0305
Fabio Tetsuo <fabioctetsuo@gmail.com>
2020-08-07 12:26:37 -0300

    feat: add pwa feature

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+const isDev = process.env.NODE_ENV === 'development';
+
+module.exports = withPWA({
+  pwa: {
+    disable: isDev,
+    dest: 'public',
+  },
   webpack: (config, { webpack }) => {
     config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
     return config;
   },
-};
+});
